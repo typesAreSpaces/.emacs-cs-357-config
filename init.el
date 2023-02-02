d0c5bdc817149502952ef1132bdd90caa14338f4
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-02-02 00:24:42 -0700

    Minor changes.

--- init.el
+++ init.el
@@ -438,35 +438,6 @@
   )
 (use-package lsp-haskell)
 
-(defun efs/org-font-setup ()
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "â¢"))))))
-
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Fira Code" :weight 'regular :height (cdr face)))
-
-  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-  (when (not (version< emacs-version "26.3"))
-    (set-face-attribute 'line-number nil :inherit 'fixed-pitch))
-  (when (not (version< emacs-version "26.3"))
-    (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)))
-
 (defun efs/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
@@ -501,9 +472,7 @@
     (lambda () (interactive) (org-todo "COMPLETED")))
 
   (define-key global-map (kbd "C-c t")
-    (lambda () (interactive) (org-todo "TODO")))
-
-  (efs/org-font-setup))
+    (lambda () (interactive) (org-todo "TODO"))))
 
 (defun org-sort-buffer ()
   "Sort all entries in the current buffer, recursively."
