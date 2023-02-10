25c68520a49e1b55306b92d3d60da6e6f3a81181
jose <jabelcastellanosjoo@unm.edu>
2023-02-10 14:17:22 -0700

    Remove snippets.

--- snippets/yasnippet-scripts.el
+++
@@ -1,6 +0,0 @@
-(defun my-yas-try-expanding-auto-snippets ()
-    (when yas-minor-mode
-      (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
-        (yas-expand))))
-
-(add-hook 'post-command-hook #'my-yas-try-expanding-auto-snippets)
