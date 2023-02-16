e075aa7a53fa166c6cee9b7c91d716417bdae148
jose <jabelcastellanosjoo@unm.edu>
2023-02-16 12:33:42 -0700

    Fix completion-styles setup.

--- init.el
+++ init.el
@@ -256,7 +256,7 @@
   ;; Configure a custom style dispatcher (see the Consult wiki)
   ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
   ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
+  (setq completion-styles '(basic substring partial-completion orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion))))
   :config
